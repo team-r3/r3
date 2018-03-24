d64cdb8f9d6060e08259f6cd79179e26c5f07c26
Luis Goncalves <lgoncalves@contentserv.com>
2018-03-24 19:13:57 +0000

    Added react-native-maps to use Google Maps API

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { Subheader } from 'react-native-material-ui';
+import MapView from 'react-native-maps';
 
 import MainContainer from '../../components/MainContainer';
 
@@ -12,8 +13,48 @@ export default class Map extends Component {
   render() {
     return (
       <MainContainer title='Map'>
-        <Text>Put the map here...</Text>
+
+        <MapView style={styleMap.map}
+          region={{
+            latitude: 37.015589,
+            longitude: -7.933262,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05
+          }}
+        >
+          
+          <MapView.Marker
+            coordinate={{
+              latitude: 37.015589,
+              longitude: -7.933262
+            }}
+            title={'Ecoponto'}
+            description={'Large aqui vidro, plástico ou papel'}
+          />
+        
+
+        </MapView>
       </MainContainer>
     );
   }
 }
+
+
+const styleMap = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  }
+});
