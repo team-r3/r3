4075852512052c3d9eb6164828c71a8c5d4a820b
samuel-h-r-nunes <samuel.h.r.nunes@gmail.com>
2018-03-24 20:53:39 +0000

    Added support for action button

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { Toolbar } from 'react-native-material-ui';
+import { ActionButton, Toolbar } from 'react-native-material-ui';
 
 const styles = StyleSheet.create({
   container: {
@@ -29,6 +29,10 @@ export default class MainContainer extends Component {
         <View style={styles.main}>
           {this.props.children}
         </View>
+        {this.props.onActionButton
+          ? <ActionButton onPress={this.props.onActionButton} />
+          : null
+        }
       </View>
     );
   }
--- src/screens/Giveaways/index.js
+++ src/screens/Giveaways/index.js
@@ -8,9 +8,13 @@ import MainContainer from '../../components/MainContainer';
  * Map screen component
  */
 export default class Giveaways extends Component {
+  onActionButton() {
+    console.log('aaaa');
+  }
+
   render() {
     return (
-      <MainContainer title='Give away'>
+      <MainContainer title='Give away' onActionButton={this.onActionButton}>
         <Subheader text="Subheader text" />
         <Text>Give-receive used stuff...</Text>
       </MainContainer>
