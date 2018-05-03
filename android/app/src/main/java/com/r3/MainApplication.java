ee2946344ca00b2da9d1564ecd8a913793e85308
Luis Goncalves <lgoncalves@contentserv.com>
2018-05-03 19:14:29 +0100

    fixed build

--- android/app/src/main/java/com/r3/MainApplication.java
+++ android/app/src/main/java/com/r3/MainApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
