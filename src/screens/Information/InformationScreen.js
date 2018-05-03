5019eb55e50ff8dff5df6746cbd446437e018fec
Ricardo Martins <rjlmartins@gmail.com>
2018-05-03 19:27:06 +0100

    Get rid of the material-ui old components and started using nativebase. Not the best components being used at the moment - work in progress.

--- src/screens/Information/InformationScreen.js
+++ src/screens/Information/InformationScreen.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { Linking, Platform, ScrollView, StyleSheet, ToastAndroid } from 'react-native'
-import { Card, ListItem, Subheader } from 'react-native-material-ui'
+import { Linking, ScrollView, StyleSheet } from 'react-native'
+import { Body, Card, List, ListItem, Icon, Text, Left } from 'native-base'
 
-import MainContainer from '../../components/MainContainer'
 import MainToolbar from '../../components/MainToolbar'
 
 const styles = StyleSheet.create({
@@ -17,120 +16,91 @@ const styles = StyleSheet.create({
 class InformationScreen extends Component {
   render () {
     return (
-      <MainContainer
-        title='Information'
-        search='Search'
-        controller={this.props.screenProps.controller}
-      >
-        <ScrollView style={styles.container}>
-          <Card>
-            <Subheader text='Paper' />
+      <ScrollView style={styles.container}>
+        <Card>
+          <List text='Paper' itemDivider >
+            <ListItem icon>
+              <Left>
+                <Icon name='checkmark-circle' style={{color: 'green'}} />
+              </Left>
+              <Body>
+                <Text>Newspapers and magazines; beverage packages; flat cartons; wrapping paper; paper bags (clean); writing paper</Text>
+              </Body>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name='close-circle' style={{color: 'red'}} />
+              </Left>
+              <Body>
+                <Text>Papers and cartons contaminated with other materials; napkin; stickers; photographs; clips and staples; packs of tobacco</Text>
+              </Body>
+            </ListItem>
+          </List>
+        </Card>
+        <Card>
+          <List text='Plastic' >
+            <ListItem icon>
+              <Left>
+                <Icon name='checkmark-circle' style={{color: 'green'}} />
+              </Left>
+              <Body>
+                <Text>Cans of beverages; cans of preserves; aluminum trays; aluminum foil; Empty aerosols</Text>
+              </Body>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name='close-circle' style={{color: 'red'}} />
+              </Left>
+              <Body>
+                <Text>Packaging contaminated with other materials; rubbers; leather; pots cutlery; caps; cookware; tools; household appliances; syringes and carpets</Text>
+              </Body>
+            </ListItem>
+          </List>
+        </Card>
+        <Card>
+          <List text='Glass' >
+            <ListItem icon>
+              <Left>
+                <Icon name='checkmark-circle' style={{color: 'green'}} />
+              </Left>
+              <Body>
+                <Text>Bottles, jars, jars of glass</Text>
+              </Body>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name='close-circle' style={{color: 'red'}} />
+              </Left>
+              <Body>
+                <Text>Mirrors; lamps; glassware; automotive glass; ceramics; porcelain; bottle and acrylic lids</Text>
+              </Body>
+            </ListItem>
+          </List>
+        </Card>
+        <Card>
+          <List text='Batteries' >
+            <ListItem icon>
+              <Left>
+                <Icon name='checkmark-circle' style={{color: 'green'}} />
+              </Left>
+              <Body>
+                <Text>Used batteries of used controls, lanterns, watches and batteries (cellphone, camera)</Text>
+              </Body>
+            </ListItem>
             <ListItem
-              divider
-              leftElement='check'
-              style={{leftElement: {color: 'green'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Newspapers and magazines; beverage packages; flat cartons; wrapping paper; paper bags (clean); writing paper'
-              }}
-              numberOfLines={'dynamic'}
-              onPress={() => {}}
-            />
-            <ListItem
-              divider
-              leftElement='close'
-              style={{leftElement: {color: 'red'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Papers and cartons contaminated with other materials; napkin; stickers; photographs; clips and staples; packs of tobacco'
-              }}
-              numberOfLines={'dynamic'}
-              onPress={() => {}}
-            />
-          </Card>
-          <Card>
-            <Subheader text='Plastic' />
-            <ListItem
-              divider
-              leftElement='check'
-              style={{leftElement: {color: 'green'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Cans of beverages; cans of preserves; aluminum trays; aluminum foil; Empty aerosols'
-              }}
-              numberOfLines={'dynamic'}
+              icon
               onPress={() => { Linking.openURL('https://www.pontoverde.pt/regras_de_separacao.php#move2resultado') }}
-            />
-            <ListItem
-              divider
-              leftElement='close'
-              style={{leftElement: {color: 'red'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Packaging contaminated with other materials; rubbers; leather; pots cutlery; caps; cookware; tools; household appliances; syringes and carpets'
-              }}
-              rightElement='info'
-              onRightElementPress={() => {
-                if (Platform.OS === 'android') {
-                  ToastAndroid.show('Right element pressed', ToastAndroid.SHORT)
-                }
-              }}
-              numberOfLines={'dynamic'}
-              onPress={() => { Linking.openURL('https://www.pontoverde.pt/regras_de_separacao.php#move2resultado') }}
-            />
-          </Card>
-          <Card>
-            <Subheader text='Glass' />
-            <ListItem
-              divider
-              leftElement='check'
-              style={{leftElement: {color: 'green'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Bottles, jars, jars of glass'
-              }}
-              numberOfLines={'dynamic'}
-              onPress={() => {}}
-            />
-            <ListItem
-              divider
-              leftElement='close'
-              style={{leftElement: {color: 'red'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Mirrors; lamps; glassware; automotive glass; ceramics; porcelain; bottle and acrylic lids'
-              }}
-              numberOfLines={'dynamic'}
-              onPress={() => {}}
-            />
-          </Card>
-          <Card>
-            <Subheader text='Batteries' />
-            <ListItem
-              divider
-              leftElement='check'
-              style={{leftElement: {color: 'green'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Used batteries of used controls, lanterns, watches and batteries (cellphone, camera)'
-              }}
-              numberOfLines={'dynamic'}
-              onPress={() => {}}
-            />
-            <ListItem
-              divider
-              leftElement='close'
-              style={{leftElement: {color: 'red'}}}
-              centerElement={{
-                primaryText: '',
-                secondaryText: 'Rechargeable batteries; car batteries; toy batteries; home appliances batteries'
-              }}
-              numberOfLines={'dynamic'}
-              onPress={() => {}}
-            />
-          </Card>
-        </ScrollView>
-      </MainContainer>
+            >
+              <Left>
+                <Icon name='close-circle' style={{color: 'red'}} />
+              </Left>
+              <Body>
+                <Text>Rechargeable batteries; car batteries; toy batteries; home appliances batteries</Text>
+              </Body>
+            </ListItem>
+          </List>
+        </Card>
+      </ScrollView>
     )
   }
 }
