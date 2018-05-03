8d432f904546ff72e7e3ed21af5f2660eba25895
Ricardo Martins <rjlmartins@gmail.com>
2018-05-03 17:35:22 +0100

    Applied Javascript standardjs to the information screen files Fixed errors pointed out by standardjs

--- src/screens/Information/InformationReducers.js
+++ src/screens/Information/InformationReducers.js
@@ -1,37 +1,37 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
 // Import action types for the information screen
 import {
   INFORMATON_UPDATE_SEARCH,
   INFORMATON_CLEAR_SEARCH
-} from './InformationActions';
+} from './InformationActions'
 
 /*
- * Reducers 
+ * Reducers
  */
 
 // Reducer for the text search value
 function search (state = '', action) {
   switch (action.type) {
     case INFORMATON_UPDATE_SEARCH:
-      return action.value;
+      return action.value
 
     case INFORMATON_CLEAR_SEARCH:
-      return '';
+      return ''
 
     default:
-      return state;
+      return state
   }
 }
 
 // Combine reducers into a single reducer for the Information screen
 export default combineReducers({
   search
-});
+})
 
-/* 
+/*
  * Selectors
  */
 
 // Get text search value
-export const getSearch = state => state.information.search;
+export const getSearch = state => state.information.search
