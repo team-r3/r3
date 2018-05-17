983ef9a5fd9d9d9ca9fed2cb6e377f717b265b53
samuel-h-r-nunes <samuel.h.r.nunes@gmail.com>
2018-05-17 22:27:20 +0100

    Updated packages and Gradle configuration
    * Updated packages:
      * react: ^16.3.2 (was ^16.3.0-alpha.1)
      * react-native: ^0.54.4 (was ^0.54.4)
      * native-base: "^2.4.4 (was ^2.4.2)
      * redux: ^4.0.0 (was ^3.7.2)
      * react-test-renderer: ^16.3.2 (was 16.3.0-alpha.3)
      * NOTE: I read the update notes and these should be safe changes,
        including "redux", which got a major version update.
    
    * Gradle-related changes:
      * Changed the way how the SDK is defined in the Gradle configuration:
        Now this is configured globally in `/android/build.gradle`, which
        gets rid of the version warnings.
      * Updated Gradle to version 4.4 (was 4.1).
      * Updated the Gradle plugin to version 3.1 (was 3.0.1).
    
    INSTALLATION STEPS:
    ===================
    rm package-lock.json
    rm node_modules
    npm install --no-optional  // (NOTE: "--no-optional" needed on Windows)
    
    TROUBLESHOOTING:
    ================
    * If there are any errors like "could not delete folder x", remove those
      folders manually, e.g.:
        rm C:\r3\android\app\build\intermediates\incremental\processDebugResources
        rm C:\r3\android\app\build\generated\source\r\debug\org\webkit
    
    * If there are errors like "unable to create folder x", try again. It
      should work on the second time.
    
    READ
    ====
    * Main motivation:
      * We should have an SDK version which is at least v26, since that will
        be the minimum to be accepted in the Play store after August 2018:
          https://github.com/facebook/react-native/issues/17287
          https://github.com/facebook/react-native/issues/19297
      * Regarding Gradle, the latest versions are required to build for the
        lastest SDKs, but will also increase compilation time a lot:
          https://github.com/facebook/react-native/issues/15448
    
    * React Native Maps installation and Gradle upgrade instructions:
        https://itnext.io/install-react-native-maps-with-gradle-3-on-android-44f91a70a395
    
    * Migration instructions for the Android Gradle Plugin v3.0.0:
        https://developer.android.com/studio/build/gradle-plugin-3-0-0-migration
    
    * List of versions of the Android Gradle Plugin and upgrade steps:
        https://developer.android.com/studio/releases/gradle-plugin

--- android/app/src/main/java/com/r3/MainApplication.java
+++ android/app/src/main/java/com/r3/MainApplication.java
@@ -5,8 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
