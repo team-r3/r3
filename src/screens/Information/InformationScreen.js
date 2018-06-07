56f5ff9a8ac8f0eff2817666523abb43dbbb7a7b
Ricardo Martins <rjlmartins@gmail.com>
2018-06-07 19:34:22 +0100

    (WIP) Information screen changes:
    * clean-up of duplicated code
    * look&feel improvements

--- src/screens/Information/InformationScreen.js
+++ src/screens/Information/InformationScreen.js
@@ -22,108 +22,36 @@ class InformationScreen extends Component {
   render () {
     return (
       <ScrollView style={styles.container}>
-        <Card>
-          <List text='Paper' itemDivider >
-            <ListItem icon>
-              <Left>
-                <Icon name='checkmark-circle' style={{color: 'green'}} />
-              </Left>
-              <Body>
-                <Text>Newspapers and magazines; beverage packages; flat cartons; wrapping paper; paper bags (clean); writing paper</Text>
-              </Body>
-            </ListItem>
-            <ListItem icon>
-              <Left>
-                <Icon name='close-circle' style={{color: 'red'}} />
-              </Left>
-              <Body>
-                <Text>Papers and cartons contaminated with other materials; napkin; stickers; photographs; clips and staples; packs of tobacco</Text>
-              </Body>
-            </ListItem>
-          </List>
-        </Card>
-        <Card>
-          <List text='Plastic' >
-            <ListItem icon>
-              <Left>
-                <Icon name='checkmark-circle' style={{color: 'green'}} />
-              </Left>
-              <Body>
-                <Text>Cans of beverages; cans of preserves; aluminum trays; aluminum foil; Empty aerosols</Text>
-              </Body>
-            </ListItem>
-            <ListItem icon>
-              <Left>
-                <Icon name='close-circle' style={{color: 'red'}} />
-              </Left>
-              <Body>
-                <Text>Packaging contaminated with other materials; rubbers; leather; pots cutlery; caps; cookware; tools; household appliances; syringes and carpets</Text>
-              </Body>
-            </ListItem>
-          </List>
-        </Card>
-        <Card>
-          <List text='Glass' >
-            <ListItem icon>
-              <Left>
-                <Icon name='checkmark-circle' style={{color: 'green'}} />
-              </Left>
-              <Body>
-                <Text>Bottles, jars, jars of glass</Text>
-              </Body>
-            </ListItem>
-            <ListItem icon>
-              <Left>
-                <Icon name='close-circle' style={{color: 'red'}} />
-              </Left>
-              <Body>
-                <Text>Mirrors; lamps; glassware; automotive glass; ceramics; porcelain; bottle and acrylic lids</Text>
-              </Body>
-            </ListItem>
-          </List>
-        </Card>
-        <Card>
-          <List text='Batteries' >
-            <ListItem icon>
-              <Left>
-                <Icon name='checkmark-circle' style={{color: 'green'}} />
-              </Left>
-              <Body>
-                <Text>Used batteries of used controls, lanterns, watches and batteries (cellphone, camera)</Text>
-              </Body>
-            </ListItem>
-            <ListItem
-              icon
-              onPress={() => { Linking.openURL('https://www.pontoverde.pt/regras_de_separacao.php#move2resultado') }}
-            >
-              <Left>
-                <Icon name='close-circle' style={{color: 'red'}} />
-              </Left>
-              <Body>
-                <Text>Rechargeable batteries; car batteries; toy batteries; home appliances batteries</Text>
-              </Body>
-            </ListItem>
 
-
-          {information.map((entry, index) => {
-            return (
-              <ListItem icon key={index}>
-              <Left>
-                <Icon name='checkmark-circle' style={{color: 'green'}} />
-              </Left>
-              <Body>
-                <Text>{entry.do.join('; ')}</Text>
-              </Body>
-            </ListItem>
-
-            )
-          })}
-    
-
-
-          </List>
-        </Card>
-
+        {information.map((entry, index) => {
+          return (
+            <Card key={index} style={{ backgroundColor: entry.color }}>
+              <List>
+                <ListItem itemDivider style={{ backgroundColor: entry.color }}>
+                  <Text>{entry.category}</Text>
+                </ListItem>
+                <ListItem avatar>
+                  <Left>
+                    <Icon name='checkmark-circle' style={{color: 'green'}} />
+                  </Left>
+                  <Body>
+                    <Text>{entry.do.join('; ')}</Text>
+                  </Body>
+                </ListItem>
+                <ListItem avatar
+                  onPress={() => { Linking.openURL('https://www.pontoverde.pt/regras_de_separacao.php#move2resultado') }}
+                >
+                  <Left>
+                    <Icon name='close-circle' style={{color: 'red'}} />
+                  </Left>
+                  <Body>
+                    <Text>{entry.dont.join('; ')}</Text>
+                  </Body>
+                </ListItem>
+              </List>
+            </Card>
+          )
+        })}
 
       </ScrollView>
     )
